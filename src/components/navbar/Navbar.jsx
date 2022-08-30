e73ddfd3b7358ea45ea9238eb532c3808af4d819
Daniel <conevskidaniel1999@gmail.com>
2022-08-30 20:43:09 +0200

    add and style navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,53 @@
 import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 const Navbar = () => {
-    return <div className="navbar">Navbar</div>;
+    return (
+        <nav className="navbar">
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder="Search..." />
+                    <SearchOutlinedIcon className="icon" />
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageOutlinedIcon className="icon" />
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <FullscreenExitOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <NotificationsNoneOutlinedIcon className="icon" />
+                        <div className="counter">3</div>
+                    </div>
+                    <div className="item">
+                        <ChatBubbleOutlineOutlinedIcon className="icon" />
+                        <div className="counter">5</div>
+                    </div>
+                    <div className="item">
+                        <ListOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <img
+                            src="https://thumbs.dreamstime.com/b/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg"
+                            alt="User Avatar"
+                            className="avatar"
+                        />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
