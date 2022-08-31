c6cbe2ec7f26ebd8c08059f8fab252f1fd6ec5ad
Daniel <conevskidaniel1999@gmail.com>
2022-08-31 19:28:18 +0200

    create page for single item

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,59 @@
+import Chart from "../../components/chart/Chart";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.scss";
 
 const Single = () => {
-    return <div>Single</div>;
+    return (
+        <div className="single">
+            <Sidebar />
+            <div className="singleContainer">
+                <Navbar />
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Edit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="item">
+                            <img
+                                src="https://thumbs.dreamstime.com/b/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg"
+                                alt="Item Image"
+                                className="itemImg"
+                            />
+                            <div className="details">
+                                <h1 className="itemTitle">John Doe</h1>
+                                <div className="detailItem">
+                                    <span className="itemKey">Email:</span>
+                                    <span className="itemValue">
+                                        johndoe@gmail.com
+                                    </span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Phone:</span>
+                                    <span className="itemValue">
+                                        +01 2345 6789
+                                    </span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Address:</span>
+                                    <span className="itemValue">
+                                        New Street 12, New York
+                                    </span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Country:</span>
+                                    <span className="itemValue">USA</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <Chart />
+                    </div>
+                </div>
+                <div className="bottom"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Single;
